c5adee702ac016f4bb83abf088562d83d29ca737
dkyoung2004 <64358951+dkyoung2004@users.noreply.github.com>
2023-06-08 15:47:21 +0900

    Update 2480.py

--- Python/beakJoon/Spare/2480.py
+++ Python/beakJoon/Spare/2480.py
@@ -1,5 +1,23 @@
 a =b =c =0
 a,b,c =map(int,input().split())
+Prize = 0
+Highest = 0
+if (a == b == c):
+    Prize = 10000+(a*1000)
+elif (a==b):
+    Prize = 1000+(a*100)
+elif (b==c):
+    Prize = 1000+(b*100)
+elif (c==a):
+    Prize = 1000+(c*100)
+else:
+    if(a > b) and (a>c):
+        Highest = a
+    elif(b > a) and (b>c):
+        Highest = b
+    else:
+        Highest = c 
+    Prize =  Highest * 100
 
-if (a == b) or (b==c) or (c==a):
-    
+print(Prize)
+
