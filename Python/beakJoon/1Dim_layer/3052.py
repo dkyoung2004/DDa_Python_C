58aa075d33b71d00c4814659358feb73cf683673
dkyoung2004 <64358951+dkyoung2004@users.noreply.github.com>
2023-06-08 22:30:02 +0900

    Update 3052.py

--- Python/beakJoon/1Dim_layer/3052.py
+++ Python/beakJoon/1Dim_layer/3052.py
@@ -1,8 +1,11 @@
 a = []
-b = []
-
-for j in range(1,43):
-    b.append(j)
-
+b = [0 for k in range(42)]
+count = 0
 for i in range(0,10):
-    a.append(int(input()))
+    a.append((int(input()))%42)
+for j in range(0,10):
+    b[a[j]] += 1
+for q in range(0,42):
+    if b[q] != 0:
+        count+=1
+print(count)
