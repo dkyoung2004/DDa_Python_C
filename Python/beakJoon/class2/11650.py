855addb2608c1c858ba37f0e9246aa8f8f80e7ea
dkyoung2004 <64358951+dkyoung2004@users.noreply.github.com>
2023-06-09 23:26:24 +0900

    Update 11650.py

--- Python/beakJoon/class2/11650.py
+++ Python/beakJoon/class2/11650.py
@@ -5,7 +5,9 @@ text = []
 n = int(input())
 b= []
 for i in range(0,n):
-    text.append(input().split())
-b = sorted(text,key = lambda x: (x[0] , x[1]))
+    text.append(list(map(int,input().split())))
+b = sorted(text)
 for j in range(0,n):
-    print(b[j])
+    print(b[j][0] , b[j][1])
+
+#마지막으로 출력만 다듬으면 완성
