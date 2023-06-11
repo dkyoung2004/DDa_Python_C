54a15d3ac7cf8c2dfe36077432dcdaa88fefe0ee
dkyoung2004 <64358951+dkyoung2004@users.noreply.github.com>
2023-06-11 15:02:54 +0900

    이것저것

--- Python/beakJoon/class2/11651.py
+++ Python/beakJoon/class2/11651.py
@@ -4,6 +4,6 @@ for i in range(0,N):
     x, y = map(int,input().split())
     arr.append((x,y))
 
-a = sorted(arr,key = lambda x:(x[0],x[1]))
+a = sorted(arr,key = lambda x:(x[1],x[0]))
 for i in range(0,N):
     print(a[i][0],a[i][1])
--- Python/beakJoon/class2/2164.py
+++ Python/beakJoon/class2/2164.py
@@ -1,2 +1,14 @@
 a = []
-n =  int(input())
+N =  int(input())
+spare = 0
+spare2 = 0
+for i in range(0,N):
+    a.append(i+1)
+for i in range(0,N-1):
+    del a[0]
+    spare = a[0]
+    spare2 = a[(int(len(a))-1)]
+    a[0] = spare2
+    a[(int(len(a))-1)] =spare
+    print(a)
+print(a)
