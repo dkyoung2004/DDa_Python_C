b53509e1e6234bdc0a414eafff61bd43d612a429
dkyoung2004 <64358951+dkyoung2004@users.noreply.github.com>
2023-06-12 15:05:13 +0900

    Update 2839.py

--- Python/beakJoon/class2/2839.py
+++ Python/beakJoon/class2/2839.py
@@ -1,1 +1,25 @@
 N = int(input())
+bonzzi = 10000
+ear_bonzzi = 5000
+i = 0
+while(N>=0):
+    if(N%5==0):
+        ear_bonzzi = (N/5)
+        ear_bonzzi += i
+    elif(N%3==0):
+        ear_bonzzi = (N/3)
+        ear_bonzzi += i
+    else:
+        ear_bonzzi = bonzzi
+    if(ear_bonzzi<bonzzi):
+        bonzzi = ear_bonzzi
+    N -= 3
+    i += 1
+    early_bonzzi = bonzzi
+
+if(bonzzi == 10000):
+    print(-1)
+else:
+    print(int(bonzzi))
+    
+
