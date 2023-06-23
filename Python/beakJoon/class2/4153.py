c14532f87f54666583109e02c6d5aaabf4ea2e6b
dkyoung2004 <64358951+dkyoung2004@users.noreply.github.com>
2023-06-23 16:56:42 +0900

    code

--- Python/beakJoon/class2/4153.py
+++ Python/beakJoon/class2/4153.py
@@ -1,1 +1,10 @@
-a, b, c = int(map(int,input().split()))
+arr = list(map(int,input().split()))
+arr = sorted(arr)
+
+while (arr[2]!=0):
+    if (((arr[0]**2)+(arr[1]**2))==(arr[2]**2)):
+        print("right")
+    else:
+        print("wrong")
+    arr = list(map(int,input().split()))
+    arr = sorted(arr)
