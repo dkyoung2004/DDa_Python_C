b779cdc89b47bc80e7b69cb8c0c7d46027ad2bda
dkyoung2004 <64358951+dkyoung2004@users.noreply.github.com>
2023-06-24 11:39:07 +0900

    finally clear

--- Python/beakJoon/class2/11866clear.py
+++ Python/beakJoon/class2/11866clear.py
@@ -1,12 +1,14 @@
+from collections import deque
+
 N , K = map(int,input().split())
 arr = []
 index = 0
 print("<",end="")
 for i in range(1,N+1):
     arr.append(i)
+arr = deque(arr)
 while len(arr) > 1:
-    index = 1
-    print(arr[index],end =", ")
-    del arr[index]
+    arr.rotate((-(K-1)))
+    print(arr.popleft(),end=", ")
 print(arr[0],end="")
 print(">")
