830c61d9603e874421ed659d08e17d915cf75e21
dkyoung2004 <64358951+dkyoung2004@users.noreply.github.com>
2023-06-29 22:38:10 +0900

    Update 9012.py

--- Python/beakJoon/class2/9012.py
+++
@@ -1,9 +0,0 @@
-N=int(input())
-
-for i in range(0,N):
-    stack = list(map(str,input()))
-    Loop = len(stack)
-    print(stack)
-    for i in range(Loop):
-        if stack[i] == a:
-            print(d)
