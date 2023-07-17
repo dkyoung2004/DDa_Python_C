f372b0b280cbfa5191a1018f960025e73323132f
dkyoung2004 <64358951+dkyoung2004@users.noreply.github.com>
2023-07-17 11:05:33 +0900

    Update 1463.py

--- Python/beakJoon/class3/1463.py
+++ Python/beakJoon/class3/1463.py
@@ -4,6 +4,6 @@ for i in range(2,X+1):
     loop[i] = loop[i-1]+1
     if i%2==0:
         loop[i] = min(loop[i], loop[i//2]+1)
-    elif i%3==0:
+    if i%3==0:
         loop[i] = min(loop[i],loop[i//3]+1)
 print(loop[X])
