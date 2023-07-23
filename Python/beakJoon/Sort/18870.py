b80f31203df8cd49b7d9db674d58543f1c22b67e
dkyoung2004 <64358951+dkyoung2004@users.noreply.github.com>
2023-07-23 12:08:14 +0900

    Update 18870.py

--- Python/beakJoon/Sort/18870.py
+++ Python/beakJoon/Sort/18870.py
@@ -1,30 +1,13 @@
+import sys
+
+input = sys.stdin.readline
+
 N = int(input())
 arr = list(map(int,input().split()))
 sort_set = list(set(arr))
+sort_set = sorted(sort_set)
+dic = {sort_set[i]: i for i in range(0,len(sort_set))}
+for i in arr:
+    print(dic[i],end=" ")
 
-def quick_sort (arr, start, end):
-    if start >= end: 
-        return
-    pv = start
-    left = start + 1
-    right = end
-    
-    while left <= right:
-    
-        while left <= end and arr[left] <= arr[pv]:
-            left += 1 
-
-        while right > start and arr[right] >= arr[pv]:
-            right -= 1
-
-        if left > right:
-            arr[right], arr[pv] = arr[pv], arr[right]
-        else:
-            arr[left], arr[right] = arr[right], arr[left]
-            
-    quick_sort(arr, 0, right -1)
-    quick_sort(arr, right + 1, end) 
-
-quick_sort(sort_set, 0, len(sort_set)-1)
-print(sort_set)
     
