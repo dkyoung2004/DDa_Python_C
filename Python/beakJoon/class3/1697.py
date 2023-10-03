d2d5b899be84c9ff44552c3ae26e7e8c19883f9e
dkyoung2004 <64358951+dkyoung2004@users.noreply.github.com>
2023-10-03 23:37:03 +0900

    Update 1697.py

--- Python/beakJoon/class3/1697.py
+++ Python/beakJoon/class3/1697.py
@@ -1,14 +1,21 @@
 from collections import deque
 
 
-a, b = map(int,input().split())
-
+n, k = map(int,input().split())
+Max = 100000
+dist = [0]* (Max+1)
 short_way = 0
 def bfs():
    q = deque()
-   q.append(a)
+   q.append(n)
+   t=0
    while q:
       x = q.popleft()
-      
-
-print(bfs(a,0))
+      if x == k:
+         print(dist[x])
+         break
+      for nx in (x-1,x+1,2*x):
+         if (0 <= nx) and (nx<= Max) and not dist[nx]:
+            dist[nx] = 1 + dist[x]
+            q.append(nx)
+bfs()
