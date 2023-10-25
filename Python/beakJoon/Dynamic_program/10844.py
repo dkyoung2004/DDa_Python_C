d348a0c8d8e1399812cf26300ad44d51c1d88052
dkyoung2004 <64358951+dkyoung2004@users.noreply.github.com>
2023-10-25 22:13:44 +0900

    Update 10844.py

--- Python/beakJoon/Dynamic_program/10844.py
+++ Python/beakJoon/Dynamic_program/10844.py
@@ -1,7 +1,16 @@
-from collections import deque
-
 N = int(input())
-initial_queue = [i for _ in range(10)]
 
-def Find():
-    loop = 0
+dp = [[0] * 10  for _ in range(N+1)]
+for i in range(1,10):
+    dp[0][i] = 1
+for i in range(1,N):
+    for j in range(0,10):
+        if(j==0):
+            dp[i][j+1] += dp[i-1][j]
+        elif(j==9):
+            dp[i][j-1] += dp[i-1][j]
+        else:
+            dp[i][j-1] += dp[i-1][j]
+            dp[i][j+1] += dp[i-1][j]
+
+print(sum(dp[N-1]))
