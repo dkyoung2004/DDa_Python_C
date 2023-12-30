79cc856b841b6b4c67a35fad29f26f8062c83b9b
dkyoung2004 <64358951+dkyoung2004@users.noreply.github.com>
2023-12-30 19:45:16 +0900

    Update 1932.py

---
+++ Python/beakJoon/class4/1932.py
@@ -0,0 +1,22 @@
+# a,b = map(int,input().split())
+# print(a+b)
+# print(a-b)
+# print(a*b)
+# print(a//b)
+# print(a%b)
+
+#나눗셈 -> % / //
+
+# # 조건문
+# age = 17
+# # height = 190
+# for i in range(1,10,2):
+#     print(i)
+
+
+
+
+a,b = map(int, input().split())
+while(a != 0 and b != 0):
+    print(a+b)
+    a,b = map(int, input().split())
