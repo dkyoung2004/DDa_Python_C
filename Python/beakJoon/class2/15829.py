b79c63a460dbbe790e0b70b91d391f4cab11a3a2
dkyoung2004 <64358951+dkyoung2004@users.noreply.github.com>
2025-11-18 22:36:31 +0900

    Create 15829.py

---
+++ Python/beakJoon/class2/15829.py
@@ -0,0 +1,13 @@
+length = int(input())
+hash = list(map(str,input()))
+key = {"a" : 1, "b" : 2, "c" : 3 , "d" : 4,"e":5,"f":6,"g":7,
+       "h" :8 ,"i":9,"j":10,"k":11,"l":12,"m":13,"n":14,"o":15,"p":16,
+       "q":17,"r":18,"s":19,"t":20,"u":21,"v":22,"w":23,"x":24,"y":25,"z":26}
+
+value = []
+result = 0
+for i in range(length):
+    value.append(key[hash[i]])
+for i in range(length):
+    result += value[i]* (31** i)
+print(result)
